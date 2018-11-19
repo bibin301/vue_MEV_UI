fcdf1f59c993840bf22a85d552995a99f55229de
bibin <bibin301@gmail.com>
2018-11-19 18:28:47 +0530

    Update Modality.vue
    removed unwanted codes

--- src/views/Modality.vue
+++ src/views/Modality.vue
@@ -5,33 +5,12 @@
         <b-button  size="sm" variant="primary" @click="addModel()">Add</b-button>
       </div>
         
-       <List-Modality :modelityLists ="modelityLists" :getActive="getActive" :EditModel="EditModel"> </List-Modality>
-        <!-- <table class="table table-bordered">
-          <thead>
-            <tr v-for="(header, index) in headerInfo" v-bind:key="index">
-              <th>{{header.name}}</th>
-              <th>{{header.code}} </th>
-              <th>{{header.description}}</th>
-              <th> {{header.active}} </th>
-              <th>{{header.action}}</th>
-            </tr>
-          </thead>
-          <tbody>
-              <tr v-for="(modelity, index) in modelityLists" v-bind:key="index">
-                   <td>{{ modelity.name }} </td>
-                  <td> {{ modelity.code }}</td>
-                  <td>{{ modelity.description }} </td>
-                  <td>
-                     <label class="switch">
-                         <input type="checkbox" @click="getActive(modelity)"  :checked="modelity.is_active" >
-                        <span class="slider round"></span>
-                    </label>
-                               
-                </td>
-                  <td>  <b-button  size="sm" variant="primary" @click="EditModel(modelity)">Modify</b-button> </td>
-              </tr>
-            </tbody>
-        </table> -->
+       <List-Modality
+        :modelityLists ="modelityLists" 
+        :getActive="getActive" 
+        :EditModel="EditModel"> 
+       </List-Modality>
+      
        
       </div>
            <Edit-model
